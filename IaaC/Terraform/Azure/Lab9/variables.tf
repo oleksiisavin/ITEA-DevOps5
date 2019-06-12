ebe8aeee5be54a3c419880537a5707b8f500ae96
oleksiisavin <50924031+oleksiisavin@users.noreply.github.com>
2019-06-12 21:44:24 +0300

    Update variables.tf

--- IaaC/Terraform/Azure/Lab9/variables.tf
+++ IaaC/Terraform/Azure/Lab9/variables.tf
@@ -1,10 +1,9 @@
 variable "prefix" {
   description = "The prefix used for all resources in this example"
   default     = "itea2019"
-}
+ }
 
 variable "location" {
   description = "The Azure location where all resources in this example should be created"
   default     = "westeurope"
-}
-
+ }
